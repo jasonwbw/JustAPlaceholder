15d7d9d44deb0f6524383e2d27349884db61ad4c
jasonwbw <jasonwbw@yahoo.com>
2016-10-12 15:12:26 +0800

    stat the unseen query's freq

--- data/just_try.py
+++ data/just_try.py
@@ -22,5 +22,27 @@ def group_by_question():
 			fo.write('%d: %s\n' % (q, ' '.join([str(item[1]) for item in groups[q]])))
 
 
+def find_valid_not_in_train():
+	from collections import Counter
+	qs = set()
+	with open('./1_reorder/invited_info_train.txt', 'r') as fp:
+		for line in fp:
+			q, u, label = map(int, line.strip().split('\t'))
+			qs.add(q)
+	new_qs = Counter()
+	total = 0
+	with open('./1_reorder/validate_nolabel.txt', 'r') as fp:
+		for line in fp:
+			q, u = map(int, line.strip().split('\t'))
+			if q not in qs:
+				total += 1
+				new_qs[q] += 1
+	with open('validate_newq.txt', 'w') as fo:
+		fo.write('total %d\n' % total)
+		for q, s in new_qs.items():
+			fo.write('%d:\t%d\n' % (q, s))
+
+
 if __name__ == '__main__':
-	group_by_question()
+	# group_by_question()
+	find_valid_not_in_train()
